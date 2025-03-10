0985213bd5e52e2f806921e9f854d509f1ad8c1d
Popkid-254 <popkidtechs@gmail.com>
2025-03-11 01:00:18 +0300

    ⭕POP KID⭕

--- set.js
+++ set.js
@@ -1,5 +1,5 @@
 global.prefix = '/'; // Set Your Preferred Prefix
-global.botName = 'POPKID-MD'; // Bot Name
+global.botName = '𝐏𝐎𝐏𝐊𝐈𝐃-𝐌𝐃'; // Bot Name
 global.botVersion = '2.0.0';
 global.botPic = 'https://files.catbox.moe/w5xf3f.jpg'; // Can use path, can also use link
 global.botToken = '7619498895:AAEuUtbqZ1uDCY9xl5HJpluf4pvd70jQMqY'; // Replace With Your Bot Token (Get it from BotFather)
@@ -7,7 +7,7 @@ global.ownerUsername = 'mouricedevs'; // Replace With Your Telegram Username(For
 global.timeZone = 'Africa/Nairobi'; // Your Time Zone
 global.ownerId = [7966970227]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
 global.footer = '*Powered by Popkid*';
-global.ytdlWeb = 'https://youtube.giftedtech.web.id';
+global.ytdlWeb = 'https://youtube.popkidtech.web.id';
 global.ytdlApi = 'https://giftedtech-ytdl-api.hf.space';
 global.giftedApi = 'https://apis.giftedtech.web.id/api'; // Gifted Api
 global.giftedKey = 'gifted'; // Gifted Api Key
