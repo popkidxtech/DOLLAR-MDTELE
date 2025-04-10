bcb62cb802ceaf651b85c95bd2f9196ce5a9720d
popkidxtech <popkidbotdeployment@gmail.com>
2025-04-10 07:39:12 +0300

    Update set.js

--- set.js
+++ set.js
@@ -1,19 +1,22 @@
 global.prefix = '.'; // Set Your Preferred Prefix
-global.botName = '𝐏𝐎𝐏𝐊𝐈𝐃-𝐌𝐃'; // Bot Name
+global.botName = 'POPKID-MD'; // Bot Name
 global.botVersion = '2.0.0';
 global.botPic = 'https://files.catbox.moe/w5xf3f.jpg'; // Can use path, can also use link
 global.botToken = '8062080160:AAE6-21ksSTirJdpTuiMCIqZaBn0SQaNaSk'; // Replace With Your Bot Token (Get it from BotFather)
 global.ownerUsername = 'Popkidtech254'; // Replace With Your Telegram Username(For Bot Owner)
 global.timeZone = 'Africa/Nairobi'; // Your Time Zone
 global.ownerId = [7966970227]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
-global.footer = '*Powered by Popkid*';
-global.ytdlWeb = 'https://youtube.popkidtech.web.id';
+global.footer = '*Powered by PopkidXtech*';
+global.ytdlWeb = 'https://ytdl.giftedtech.web.id';
 global.ytdlApi = 'https://giftedtech-ytdl-api.hf.space';
-global.giftedApi = 'https://apis.giftedtech.web.id/api'; // Gifted Api
+global.giftedApi = 'https://api.giftedtech.web.id/api'; // Gifted Api
 global.giftedKey = 'gifted'; // Gifted Api Key
 global.maherApi = 'https://api.nexoracle.com'; // NexOracle Api
 global.maherKey = ''; // NexOracle Api Key
 global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
+global.giftedWaChannel = 'https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3l';
+global.giftedRepo = "https://github.com/Popkiddevs/POPKID-XTECH";
+global.giftedApiRepo = "https://api.github.com/repos/Popkiddevs/POPKID-XTECH";
 
 global.giftechMess = {
     internalerr: 'Internal Server Error!',
